7c1580a8beaf96da96e47b2804ea13dfa625aac7
Cecy Flores <anacecyflores1@gmail.com>
2023-04-30 21:01:58 -0500

    Completed functionality for sentence stems

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-import { themeStems, themeIdea, evidenceStem, reasoningStem } from "./stems.js";
+import {
+  themeStems,
+  // themeWords,
+  evidenceStem,
+  reasoningStem,
+  interactStems,
+  // interactWords,
+  conclusionStem,
+  interpretationStems,
+  // interpretationWords,
+  comparisonStems,
+  // comparisonWords,
+  povStems,
+  // povWords,
+} from "./stems.js";
 // -------- RL BUTTONS --------
 let rlButton2 = document.getElementById("reading_lit2");
 let rlButton3 = document.getElementById("reading_lit3");
@@ -12,25 +26,87 @@ let result = document.getElementById("result_box");
 // RL2 - determine theme or central idea
 function readingLit2() {
   // claim
-  let theme =
-    themeStems[Math.floor(Math.random() * themeStems.length)] +
-    themeIdea[Math.floor(Math.random() * themeIdea.length)];
+  let claim = themeStems[Math.floor(Math.random() * themeStems.length)];
+  // themeWords[Math.floor(Math.random() * themeWords.length)].toLowerCase();
   // evidence
   let evidence = evidenceStem[Math.floor(Math.random() * evidenceStem.length)];
   // reasoning
   let reasoning =
     reasoningStem[Math.floor(Math.random() * reasoningStem.length)];
+  // conclusion
+  let conclusion =
+    conclusionStem[Math.floor(Math.random() * conclusionStem.length)];
   // result
-  result.innerHTML = `${theme} ${evidence} [INSERT QUOTE HERE]. ${reasoning}`;
+  result.innerHTML = `${claim} ${evidence} <span class="quote">[INSERT QUOTE HERE]</span>. ${reasoning}. ${conclusion}.`;
 }
 // RL3 - analyze how dialogue propels the story
-function readingLit3() {}
-// RL4 - determine the meaning of words and phrases (figurative / connotative meanings, tone, analogies, allusions...)
-function readingLit4() {}
+function readingLit3() {
+  // claim
+  let claim = interactStems[Math.floor(Math.random() * interactStems.length)];
+  // interactWords[
+  //   Math.floor(Math.random() * interactWords.length)
+  // ].toLowerCase();
+  // evidence
+  let evidence = evidenceStem[Math.floor(Math.random() * evidenceStem.length)];
+  // reasoning
+  let reasoning =
+    reasoningStem[Math.floor(Math.random() * reasoningStem.length)]; // conclusion
+  let conclusion =
+    conclusionStem[Math.floor(Math.random() * conclusionStem.length)];
+  // result
+  result.innerHTML = `${claim} ${evidence} <span class="quote">[INSERT QUOTE HERE]</span>. ${reasoning}. ${conclusion}.`;
+}
+// RL4 - determine the meaning of words and phrases (figurative / connotative meanings, tone, analogies, allusions_____)
+function readingLit4() {
+  // claim
+  let claim =
+    interpretationStems[Math.floor(Math.random() * interpretationStems.length)];
+  // interpretationWords[
+  //   Math.floor(Math.random() * interpretationWords.length)
+  // ].toLowerCase();
+  // evidence
+  let evidence = evidenceStem[Math.floor(Math.random() * evidenceStem.length)];
+  // reasoning
+  let reasoning =
+    reasoningStem[Math.floor(Math.random() * reasoningStem.length)]; // conclusion
+  let conclusion =
+    conclusionStem[Math.floor(Math.random() * conclusionStem.length)];
+  // result
+  result.innerHTML = `${claim} ${evidence} <span class="quote">[INSERT QUOTE HERE]</span>. ${reasoning}. ${conclusion}.`;
+}
 // RL5 - compare and contrast structure of 2 or more texts, analyze how differing structure contributes to meaning and style
-function readingLit5() {}
+function readingLit5() {
+  // claim
+  let claim =
+    comparisonStems[Math.floor(Math.random() * comparisonStems.length)];
+  // comparisonWords[
+  //   Math.floor(Math.random() * comparisonWords.length)
+  // ].toLowerCase();
+  // evidence
+  let evidence = evidenceStem[Math.floor(Math.random() * evidenceStem.length)];
+  // reasoning
+  let reasoning =
+    reasoningStem[Math.floor(Math.random() * reasoningStem.length)]; // conclusion
+  let conclusion =
+    conclusionStem[Math.floor(Math.random() * conclusionStem.length)];
+  // result
+  result.innerHTML = `${claim} ${evidence} <span class="quote">[INSERT QUOTE HERE]</span>. ${reasoning}. ${conclusion}.`;
+}
 // RL6 - analyze how differences in POV of character or  reader creates effects such as suspense or humor
-function readingLit6() {}
+function readingLit6() {
+  // claim
+  let claim = povStems[Math.floor(Math.random() * povStems.length)];
+  // povWords[Math.floor(Math.random() * povWords.length)].toLowerCase();
+  // evidence
+  let evidence = evidenceStem[Math.floor(Math.random() * evidenceStem.length)];
+  // reasoning
+  let reasoning =
+    reasoningStem[Math.floor(Math.random() * reasoningStem.length)]; // conclusion
+  let conclusion =
+    conclusionStem[Math.floor(Math.random() * conclusionStem.length)];
+  // result
+  result.innerHTML = `${claim} ${evidence} <span class="quote">[INSERT QUOTE HERE]</span>. ${reasoning}. ${conclusion}.`;
+}
 
 // -------- EVENT LISTENERS --------
 rlButton2.addEventListener("click", readingLit2);
